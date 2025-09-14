cd3a6f14927adc336cad92d7378b13b93d635751
blysshua <Dreamworks_Studios@protonmail.com>
2025-09-14 17:23:47 +1000

    Update dashboard.sh

--- dashboard.sh
+++ dashboard.sh
@@ -10,6 +10,10 @@ tmux send-keys -t $SESSION:0.0 "bash ~/dashboard/active.sh;  tmux kill-session -
 tmux split-window -h -t $SESSION:0
 tmux send-keys -t $SESSION:0.1 "btop; tmux kill-session -t $SESSION" C-m
 
+tmux split-window -v -t $SESSION:0.1
+tmux send-keys -t $SESSION:0.2 "sudo iftop; tmux kill-session -t $SESSION" C-m
+
+
 tmux bind-key q kill-session
 
 tmux attach -t $SESSION
